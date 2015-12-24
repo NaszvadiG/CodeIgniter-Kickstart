9cc691e50cd9fac5ac2cf0ae9346bcc89a1f7118
gotphp <michaelfountain@gmail.com>
2015-12-23 23:28:29 -0800

    Home controller index view

---
+++ application-3.0.3/views/home/home_index.php
@@ -0,0 +1,18 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+<div id="container">
+	<h1>Welcome to CodeIgniter Kickstart!</h1>
+
+	<div id="body">
+		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+
+		<p>If you would like to edit this page you'll find it located at:</p>
+		<code>application-3.0.3/views/home/home_index.php</code>
+
+		<p>The corresponding controller for this page is found at:</p>
+		<code>application-3.0.3/controllers/Home.php</code>
+
+		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="http://www.codeigniter.com/user_guide/">User Guide</a>.</p>
+	</div>
+
+</div>
